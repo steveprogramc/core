b493badcc115eca4ba0eecd230d7d6c83029dd2b
Robert Resch <robert@resch.dev>
2023-10-11 07:38:01 +0000

    Add all units of measurement for selector in scrape

--- homeassistant/components/scrape/config_flow.py
+++ homeassistant/components/scrape/config_flow.py
@@ -12,6 +12,7 @@ from homeassistant.components.rest.data import DEFAULT_TIMEOUT
 from homeassistant.components.rest.schema import DEFAULT_METHOD, METHODS
 from homeassistant.components.sensor import (
     CONF_STATE_CLASS,
+    DEVICE_CLASS_UNITS as SENSOR_DEVICE_CLASS_UNITS,
     DOMAIN as SENSOR_DOMAIN,
     SensorDeviceClass,
     SensorStateClass,
@@ -34,7 +35,6 @@ from homeassistant.const import (
     CONF_VERIFY_SSL,
     HTTP_BASIC_AUTHENTICATION,
     HTTP_DIGEST_AUTHENTICATION,
-    UnitOfTemperature,
 )
 from homeassistant.core import async_get_hass
 from homeassistant.helpers import config_validation as cv, entity_registry as er
@@ -129,7 +129,15 @@ SENSOR_SETUP = {
     ),
     vol.Required(CONF_UNIT_OF_MEASUREMENT, default=NONE_SENTINEL): SelectSelector(
         SelectSelectorConfig(
-            options=[NONE_SENTINEL] + sorted([cls.value for cls in UnitOfTemperature]),
+            options=[NONE_SENTINEL]
+            + sorted(
+                {
+                    str(unit)
+                    for units in SENSOR_DEVICE_CLASS_UNITS.values()
+                    for unit in units
+                    if unit is not None
+                }
+            ),
             custom_value=True,
             mode=SelectSelectorMode.DROPDOWN,
             translation_key="unit_of_measurement",
